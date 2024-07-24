fc27b988948b2381b9cee5eecf211fe7a127742e
Ptolemy Henson <ptolemy2002@gmail.com>
2024-07-24 11:25:11 -0400

    Set up the library

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,11 +1,29 @@
-import {Example} from "@local/lib";
+import { useState } from "react";
+import { useMountEffect, useUnmountEffect } from "@ptolemy2002/react-mount-effects";
 
 function App() {
-  return (
-    <div className="App">
-      <Example />
-    </div>
-  );
+    const [showA, setShowA] = useState(true);
+
+    return (
+        <div>
+            {showA ? <A /> : <B />}
+            <button onClick={() => setShowA(!showA)}>Toggle</button>
+        </div>
+    );
 }
 
 export default App;
+
+function A() {
+    useMountEffect(() => console.log("A mounted"));
+    useUnmountEffect(() => console.log("A unmounted"));
+
+    return <div>A</div>;
+}
+
+function B() {
+    useMountEffect(() => console.log("B mounted"));
+    useUnmountEffect(() => console.log("B unmounted"));
+
+    return <div>B</div>;
+}
